fb9bc4ae964ed327a07fbdc507692fc0f9e1d276
Robert Hjalmers <robert.hjalmers@gmail.com>
2016-12-19 17:31:45 +0100

    working fixed column position

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,9 +37,10 @@ export class AppComponent {
     }
   ];
 
-  public row = [{xl:12},{xl:12}];
-  public cards = [this.row];
+  public columns = [{xl:4},{xl:4}];
+  public cards = [this.columns];
   public edit:boolean = false;
+  public autoFillColumns:boolean = true;
 
   public editLayout = function(){
     this.edit = !this.edit;
@@ -53,41 +54,49 @@ export class AppComponent {
     this.cards.splice(this.cards.length-1,1);
   };
 
-  public addColumn = function(row){
-    row.push({xl:12});
+  public addColumn = function(columns){
+    columns.push({xl:4});
   };
 
-  public removeColumn = function(row){
-    row.splice(row.length-1,1);
+  public removeColumn = function(columns){
+    columns.splice(columns.length-1,1);
   };
 
-  public getRowWidth = function(row:Array){
-    console.log(row);
+  public getRowWidth = function(columns:Array<any>){
+    console.log(columns);
     let sum = 0;
-    for(let i = 0;i < row.length; i++){
-      console.log(row[i]);
-      sum += row[i].xl;
+    for(let i = 0;i < columns.length; i++){
+      console.log(columns[i]);
+      sum += columns[i].xl;
     }
 
     console.log(sum);
     return sum;
-    //console.log(row.reduce((previous, current) => previous.xl+current.xl, {xl:0}));
+    //console.log(columns.reduce((previous, current) => previous.xl+current.xl, {xl:0}));
   };
-  public grow = function(column, row:Array){
-    let rowWidth = this.getRowWidth(row);
-    if(rowWidth < 12 && column.xl < (12-row.length+1)) {
+  public grow = function(column, columns:Array<any>, columnIndex:number){
+    let columnsWidth = this.getRowWidth(columns);
+    if(columnsWidth < 12 && column.xl < (12-columns.length+1)) {
+      console.log('case 1');
       column.xl = column.xl + 1;
-    } else if(column.xl < (12-row.length+1)) {
-      row[1].xl = row[1].xl - 1;
+    } else if(column.xl < (12-columns.length+1) && columnIndex+1 < columns.length && columns[columnIndex+1].xl > 1) {
+      console.log('case 2');
+      columns[columnIndex+1].xl = columns[columnIndex+1].xl - 1;
+      column.xl = column.xl + 1;
+    } else if (columnsWidth < 12){
+      console.log('case 3');
+      columns[columnIndex-1].xl = columns[columnIndex-1].xl - 1;
       column.xl = column.xl + 1;
     }
   };
-  public shrink = function(column, row:Array){
-    let rowWidth = this.getRowWidth(row);
-    if(rowWidth > 1 && column.xl > 1) {
+  public shrink = function(column, columns:Array<any>, columnIndex:number){
+    let columnsWidth = this.getRowWidth(columns);
+    if(columnsWidth > 1 && column.xl > 1) {
       column.xl = column.xl - 1;
-      if(rowWidth <= 12){
-        row[1].xl = row[1].xl + 1;
+      if(columnsWidth <= 12 && columnIndex+1 < columns.length && this.autoFillColumns){
+        columns[columnIndex+1].xl = columns[columnIndex+1].xl + 1;
+      } else if(this.autoFillColumns && columns.length > 1) {
+        columns[columnIndex-1].xl = columns[columnIndex-1].xl + 1;
       }
     }
   };
