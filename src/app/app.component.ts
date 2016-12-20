d8f05a39f6cbca974dd2929e207b950aa99902d7
hjalmers <robert.hjalmers@gmail.com>
2016-12-20 16:31:09 +0100

    flex based

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import {ChangeDetectorRef} from '@angular/core'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app works!';
--- src/app/bg-widget/bg-widget.component.ts
+++ src/app/bg-widget/bg-widget.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'bg-widget',
   templateUrl: './bg-widget.component.html',
-  styleUrls: ['./bg-widget.component.css']
+  styleUrls: ['./bg-widget.component.scss']
 })
 export class BgWidgetComponent implements OnInit {
 
