5060b9212c02314dfd7a0fa8c8e49f0f6a840ec5
hjalmers <robert.hjalmers@gmail.com>
2017-05-02 15:22:37 +0200

    updated gh-pages

--- src/app/widget.ts
+++
@@ -1,7 +0,0 @@
-export interface Widget {
-  xs?:number,
-  sm?:number,
-  md?:number,
-  lg?:number,
-  xl?:number
-}
--- styles.58e065928ed8ebd0b582.bundle.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([1,2],{351:function(n,o){},352:function(n,o){},675:function(n,o,c){c(351),n.exports=c(352)}},[675]);
-//# sourceMappingURL=styles.58e065928ed8ebd0b582.bundle.map
